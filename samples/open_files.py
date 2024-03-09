b7774f9807e50c48be74395260c7170297525d22
phosp4 <56543331+phosp4@users.noreply.github.com>
2024-03-09 12:02:56 +0100

    Add files via upload

---
+++ samples/open_files.py
@@ -0,0 +1,28 @@
+termsO = open("../res/terms.txt","r")
+descrO = open("../res/definitions.txt","r")
+cluesO = open("../res/clues.txt","r")
+
+termsR = termsO.readlines()
+descrR = descrO.readlines()
+cluesR = cluesO.readlines()
+
+termsL = []
+descrL = []
+cluesL = []
+
+for i in range(len(termsR)):
+    #t = termsR[i]
+    #d = descrR[i]
+    #c = cluesR[i]
+    termsL.append((termsR[i])[0:-1])
+    descrL.append((descrR[i])[0:-1])
+    cluesL.append((cluesR[i])[0:-1])
+    
+
+print(termsL)
+print(descrL)
+print(cluesL)
+
+termsO.close()
+descrO.close()
+cluesO.close()
